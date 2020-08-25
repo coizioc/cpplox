a2b6a49ddfdb3914c9336ee8e4a95966634ca9d8
coizioc <kegeshan@gmail.com>
2020-08-25 18:06:55 -0400

    Remove clang-format.sh (moved to git hook).

--- clang-format.sh
+++
@@ -1,6 +0,0 @@
-mapfile -t CPP_FILES < <(find . -type f -name "*.[ch]pp")
-printf "%s\n" "${CPP_FILES[@]}"
-for file in "${CPP_FILES[@]}"
-do
-   clang-format -style="{BasedOnStyle: Google, IndentWidth: 4}" -i $file
-done
